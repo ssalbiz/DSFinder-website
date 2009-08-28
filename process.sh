f69e5baf86993c89e86a44efac27413bcd4cc17b
Syed S. Albiz <ssalbiz@uwaterloo.ca>
2009-08-28 11:33:43 -0400

    final edit (hopefully)

--- process.sh
+++ process.sh
@@ -6,14 +6,9 @@ BENCHMARKS_DIR= ##
 ##
 
 FILE=$1
-echo $FILE >> ${STAGING_DIR}/tout
-unzip $FILE -z -d ${STAGING_DIR}/ 
-BASE=`basename $STAGING_DIR`
-TMP=`find ${STAGING_DIR}/ -name *\.class |  sed 's/Binary\ file//g' | sed 's/matches//g' | sed 's/\.class//g' | sed "s/.*$BASE\///g" | sed 's/\//\./g' | head -1`
-echo $TMP >> ${STAGING_DIR}/tout
-export MAIN_CLASS=$TMP
 export PACKAGE=$2
 export PROJECT_ROOT=$STAGING_DIR
+export CLASSES_DIR=$FILE
 
 ##USE LOCAL DSFINDER
 cd ${BENCHMARKS_DIR}/scripts
