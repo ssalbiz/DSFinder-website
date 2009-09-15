826f2cc3e08ed9fdcaaa05b4dbd3bf999433836d
Syed S. Albiz <ssalbiz@uwaterloo.ca>
2009-09-14 23:06:44 -0400

    output changes

--- tool.php
+++ tool.php
@@ -45,8 +45,8 @@
    echo "Received ", basename($file), "\n";
    
 
-   $cmd = 'java -Xmx1800M -cp /usr/lib/jvm/java-6-openjdk/jre/lib/rt.jar:/usr/lib/jvm/java-6-openjdk/jre/lib/jce.jar:/usr/lib/jvm/java-6-openjdk/jre/lib/jsse.jar:/usr/lib/jvm/java-6-openjdk/lib/tools.jar:/xtra/benchmark_sources/scripts/DSFinder-20090828.jar:/usr/share/java/asm3.jar:/usr/share/java/asm3-commons.jar:/usr/share/java/asm3-tree.jar:/xtra/benchmark_sources/scripts/jargs.jar:' . escapeshellarg($file) . ' ca.patricklam.DSFinder --classpath=/usr/lib/jvm/java-6-openjdk/jre/lib/rt.jar:/usr/lib/jvm/java-6-openjdk/jre/lib/jce.jar:/usr/lib/jvm/java-6-openjdk/jre/lib/jsse.jar:/usr/lib/jvm/java-6-openjdk/lib/tools.jar:/xtra/benchmark_sources/scripts/DSFinder-20090828.jar:/usr/share/java/asm3.jar:/usr/share/java/asm3-commons.jar:/usr/share/java/asm3-tree.jar:/xtra/benchmark_sources/scripts/jargs.jar:' . escapeshellarg($file) . '-s -p --include='. escapeshellarg($package) .' --title=/tmp/staging.log';
-   echo $cmd;
+   $cmd = 'java -Xmx1800M -cp /usr/lib/jvm/java-6-openjdk/jre/lib/rt.jar:/usr/lib/jvm/java-6-openjdk/jre/lib/jce.jar:/usr/lib/jvm/java-6-openjdk/jre/lib/jsse.jar:/usr/lib/jvm/java-6-openjdk/lib/tools.jar:/xtra/benchmark_sources/scripts/DSFinder-20090828.jar:/usr/share/java/asm3.jar:/usr/share/java/asm3-commons.jar:/usr/share/java/asm3-tree.jar:/xtra/benchmark_sources/scripts/jargs.jar:' . escapeshellarg($file) . ' ca.patricklam.DSFinder --classpath=/usr/lib/jvm/java-6-openjdk/jre/lib/rt.jar:/usr/lib/jvm/java-6-openjdk/jre/lib/jce.jar:/usr/lib/jvm/java-6-openjdk/jre/lib/jsse.jar:/usr/lib/jvm/java-6-openjdk/lib/tools.jar:/xtra/benchmark_sources/scripts/DSFinder-20090828.jar:/usr/share/java/asm3.jar:/usr/share/java/asm3-commons.jar:/usr/share/java/asm3-tree.jar:/xtra/benchmark_sources/scripts/jargs.jar:' . escapeshellarg($file) . ' -s -p --include='. escapeshellarg($package) .' --title=/tmp/staging.log';
+   echo "$cmd\n";
    system(escapeshellcmd($cmd), $rc);
    echo $rc;
 
